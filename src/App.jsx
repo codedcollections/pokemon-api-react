54cba1d1b07edafc53ee629a63988c13f752eb89
codedcollections <ma.unander@gmail.com>
2025-11-17 13:42:42 +0100

    Add functionality to fetch pokemon via API

--- src/App.jsx
+++ src/App.jsx
@@ -4,9 +4,17 @@ import "./App.css"
 function App() {
   const [count, setCount] = useState(0)
 
+  const getPokemon = async () => {
+    let response = await fetch(`https://pokeapi.co/api/v2/pokemon?limit=151`)
+    let json = await response.json()
+    let answer = JSON.stringify(json.results)
+    console.log("pokemons read:" + answer)
+  }
+
   return (
     <>
       <h1>gello</h1>
+      <button onClick={getPokemon}>fetch API pokemon</button>
     </>
   )
 }
