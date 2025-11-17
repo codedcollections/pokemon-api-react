b69ed89a2d745389b5ba9cbefd0b4ad8980e7a74
codedcollections <ma.unander@gmail.com>
2025-11-17 14:03:49 +0100

    Add functionality show fetched pokemon names

--- src/App.jsx
+++ src/App.jsx
@@ -2,19 +2,24 @@ import { useState } from "react"
 import "./App.css"
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [count, setCount] = useState(false)
+  const [pokemonArr, setPokemonArr] = useState(null)
 
   const getPokemon = async () => {
     let response = await fetch(`https://pokeapi.co/api/v2/pokemon?limit=151`)
     let json = await response.json()
-    let answer = JSON.stringify(json.results)
-    console.log("pokemons read:" + answer)
+    console.log(json)
+    //let answer = JSON.stringify(json.results)
+    setPokemonArr(json.results)
+
+    setCount(true)
   }
 
   return (
     <>
       <h1>gello</h1>
       <button onClick={getPokemon}>fetch API pokemon</button>
+      {pokemonArr && pokemonArr.map((pokemon) => <h1>{pokemon.name}</h1>)}
     </>
   )
 }
